2db098e58da2e45ed4cff722477c9241268c5949
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-05-03 17:34:14 +0800

    feat: add initial impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,39 @@
-use client_api::Client;
+pub mod error;
 
+use crate::error::Error;
+use client_api::Client;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "snake_case")]
 pub enum RecurringInterval {
     Month,
     Year,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "snake_case")]
 pub enum SubscriptionPlan {
     Pro,
     Team,
 }
 
 pub trait PaymentClient {
-    async fn stripe_payment_link(
+    fn stripe_payment_link(
         &self,
         workspace_id: &str,
-        success_url: &str,
         recurring_interval: RecurringInterval,
         workspace_subscription_plan: SubscriptionPlan,
-    ) -> Result<String, String>;
+    ) -> impl std::future::Future<Output = Result<String, Error>> + Send;
 }
 
 impl PaymentClient for Client {
     async fn stripe_payment_link(
         &self,
         _workspace_id: &str,
-        _success_url: &str,
         _recurring_interval: RecurringInterval,
         _workspace_subscription_plan: SubscriptionPlan,
-    ) -> Result<String, String> {
+    ) -> Result<String, Error> {
         todo!()
     }
 }
--- tests/api/payment_link.rs
+++ tests/api/payment_link.rs
@@ -1,7 +1,29 @@
 use appflowy_cloud_billing_client::PaymentClient;
+use client_api_test_util::generate_unique_registered_user_client;
 
 #[tokio::test]
 async fn test_payment_link() {
-    let client: client_api::Client = todo!();
-    let _ = client.stripe_payment_link().await;
+    let (client, _) = generate_unique_registered_user_client().await;
+    let workspace_id = client
+        .get_workspaces()
+        .await
+        .unwrap()
+        .0
+        .first()
+        .unwrap()
+        .workspace_id
+        .to_string();
+
+    let url = client
+        .stripe_payment_link(
+            &workspace_id,
+            appflowy_cloud_billing_client::RecurringInterval::Month,
+            appflowy_cloud_billing_client::SubscriptionPlan::Pro,
+        )
+        .await
+        .unwrap();
+
+    let a = client.base_url;
+
+    //assert!(url.starts_with("https://checkout.stripe.com/"));
 }
